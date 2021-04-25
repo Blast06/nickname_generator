8ab2a9a00bc70be25c7822114574c2d8ce9f4918
Blast06 <lacalculadora06@hotmail.com>
2021-04-25 11:16:01 -0400

    deleted everything

--- android/app/src/main/kotlin/com/nickname/generator/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.nickname.generator
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/com/nickname/nickname_generator/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.nickname.nickname_generator
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}

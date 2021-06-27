cdcb53a37b7b9de0b6ae43cba21938638805984b
blast06 <lacalculadora06@hotmail.com>
2021-06-27 09:37:25 -0400

    changed app color

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -19,7 +19,6 @@ class HomePage extends StatelessWidget {
   final hc = Get.put(HomeController());
   final TextEditingController nameController = TextEditingController();
 
-  
   final bannerAd = BannerAd(
     // controller: bannerController,
     size: BannerSize.ADAPTIVE,
@@ -32,94 +31,98 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: (){
+      onTap: () {
         if (Platform.isIOS) hc.hideKeyboard(context);
       },
       child: Scaffold(
-          backgroundColor: Colors.yellow[800],
-          // appBar: AppBar(
-          //   title: Text('Nickname Generator'),
-          // ),
-          body: Column(
-            children: [
-              const SizedBox(
-                height: 80,
-              ),
-              Text(MyAdmob.getAppName(), style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-              GetBuilder<HomeController>(
-                builder: (_) => Container(
-                  margin: EdgeInsets.only(top: 30, left: 18, right: 18),
-                  child: CustomSearchText(
-                    text: hc.nickName,
-                    enable: true,
-                    callback: () {
-                      nameController.text = hc.nickName;
-                      // TODO: Fix bug when generating new nickname and does not change in textbox...
-                    },
-                    controller: nameController,
-                    callback2: (text) {
-                      _.changeNickName(text);
-                    },
-                  ),
+        backgroundColor: appThemeData.highlightColor,
+        // appBar: AppBar(
+        //   title: Text('Nickname Generator'),
+        // ),
+        body: Column(
+          children: [
+            const SizedBox(
+              height: 80,
+            ),
+            Text(
+              MyAdmob.getAppName(),
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            GetBuilder<HomeController>(
+              builder: (_) => Container(
+                margin: EdgeInsets.only(top: 30, left: 18, right: 18),
+                child: CustomSearchText(
+                  text: hc.nickName,
+                  enable: true,
+                  callback: () {
+                    nameController.text = hc.nickName;
+                    // TODO: Fix bug when generating new nickname and does not change in textbox...
+                  },
+                  controller: nameController,
+                  callback2: (text) {
+                    _.changeNickName(text);
+                  },
                 ),
               ),
+            ),
 
-              //TODO: Use svg icon
-              GetBuilder<HomeController>(
-                builder: (_) => Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    IconButton(
-                      icon: Icon(Icons.restore_sharp),
-                      onPressed: () => hc.generateRandomNickName(),
-                    ),
-                    IconButton(
-                        icon: Icon(Icons.copy_rounded),
-                        onPressed: () {
-                          Clipboard.setData(ClipboardData(text: hc.nickName));
-                           Get.snackbar("snackbar_download_title".tr,
+            //TODO: Use svg icon
+            GetBuilder<HomeController>(
+              builder: (_) => Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.autorenew),
+                    onPressed: () => hc.generateRandomNickName(),
+                  ),
+                  IconButton(
+                      icon: Icon(Icons.copy_rounded),
+                      onPressed: () {
+                        Clipboard.setData(ClipboardData(text: hc.nickName));
+                        Get.snackbar("snackbar_download_title".tr,
                             "snackbar_download_message".tr,
                             snackPosition: SnackPosition.BOTTOM,
                             backgroundColor: appThemeData.accentColor);
-                        }),
-                  ],
+                      }),
+                ],
+              ),
+            ),
+            // const SizedBox(
+            //   height: 10,
+            // ),
+
+            Expanded(
+              child: GetBuilder<HomeController>(
+                builder: (_) => ListView.builder(
+                  physics: const BouncingScrollPhysics(),
+                  itemCount: symbols_first_page.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    int lastOne = symbols_first_page.length - 1;
+                    // if (index == lastOne) {
+                    //   return Container(
+                    //       margin: EdgeInsets.only(left: 7, right: 7),
+                    //       child: TextButton(
+                    //         onPressed: () {},
+                    //         child: Text('More'),
+                    //       ));
+                    // }
+
+                    // return Text('holaa');
+                    return Container(
+                      margin: EdgeInsets.only(left: 7, right: 7),
+                      child: SlimCard(
+                        text: _.replaceString(
+                            symbols_first_page[index].symbol, _.nickName),
+                        widget: Icon(Icons.copy),
+                      ),
+                    );
+                  },
                 ),
               ),
-              // const SizedBox(
-              //   height: 10,
-              // ),
-
-              Expanded(
-                child: GetBuilder<HomeController>(
-                  builder: (_) => ListView.builder(
-                    physics: const BouncingScrollPhysics(),
-                    itemCount: symbols_first_page.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      int lastOne = symbols_first_page.length - 1;
-                      // if (index == lastOne) {
-                      //   return Container(
-                      //       margin: EdgeInsets.only(left: 7, right: 7),
-                      //       child: TextButton(
-                      //         onPressed: () {},
-                      //         child: Text('More'),
-                      //       ));
-                      // }
-
-                      // return Text('holaa');
-                      return Container(
-                        margin: EdgeInsets.only(left: 7, right: 7),
-                        child: SlimCard(
-                            text:
-                                _.replaceString(symbols_first_page[index].symbol, _.nickName), widget: Icon(Icons.copy),),
-                      );
-                    },
-                  ),
-                ),
-              ),
-            ],
-          ),
-          bottomNavigationBar: BottomBar(),
-        
+            ),
+          ],
+        ),
+        bottomNavigationBar: BottomBar(),
       ),
     );
   }

4d88bef88c712356cfc2d873ab84eb5bc5ed6a3a
blast06 <lacalculadora06@hotmail.com>
2021-07-13 22:22:06 -0400

    changed theme colors

--- lib/utils/apptheme.dart
+++ lib/utils/apptheme.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:generator/utils/colors.dart';
 
 final ThemeData appThemeData = ThemeData(
-  primaryColor: Color(0xff25F4EE),
-  accentColor: Color(0xffFE2C55),
+  primaryColor: Color(0xff003566),
+  accentColor: caribbean_color,
   splashColor: Colors.black,
   highlightColor: Colors.white,
 //   fontFamily: GoogleFonts.lato().fontFamily,

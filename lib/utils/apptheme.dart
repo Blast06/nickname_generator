80307b397b84d9e98e06f2292c50152a96c254c6
blast06 <lacalculadora06@hotmail.com>
2021-10-03 16:33:33 -0400

    changed apps color

--- lib/utils/apptheme.dart
+++ lib/utils/apptheme.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:generator/utils/colors.dart';
 
 final ThemeData appThemeData = ThemeData(
-  primaryColor: Color(0xff06D6A0),
-  accentColor: Color(0xffFFD166),
-  splashColor: Color(0xffEF476F),
-  highlightColor: Color(0xff073B4C),
+  primaryColor: Color(0xff462d54),
+  accentColor: Color(0xfffe0968),
+  splashColor: Color(0xffdff1ff),
+  highlightColor: Color(0xffff1cd7),
+  backgroundColor: Color(0xff462d54),
+  appBarTheme: AppBarTheme(backgroundColor: Color(0xff7d6889)),
 //   fontFamily: GoogleFonts.lato().fontFamily,
   textTheme: TextTheme(
     headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
